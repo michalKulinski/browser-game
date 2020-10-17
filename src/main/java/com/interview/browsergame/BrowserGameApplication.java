c69171dbe868fa6ea5cff022aa5012998a614df0
Michal Kulinski <you@example.com>
2020-10-17 14:46:29 +0200

    Innitial project

---
+++ src/main/java/com/interview/browsergame/BrowserGameApplication.java
@@ -0,0 +1,13 @@
+package com.interview.browsergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowserGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/browsergame/BrowserGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.browsergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
